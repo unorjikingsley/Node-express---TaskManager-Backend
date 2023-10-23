b782579c5b2b7386cfbe132315a8e72702d6ed5c
unorji kingsley <unorjikingsley@gmail.com>
2023-10-23 08:00:19 +0100

    Setup routes, controllers skeleton

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes/tasks');
 
-// app.use()
+//middleware
+app.use(express.json());
 
 //routes
 app.get('/hello', (req, res) => 
   res.send('first node project')
 )
+
+app.use('api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks', (req, res) => {})                 - get all the tasks
+// app.post('/api/v1/tasks', (req, res) => {})                - create a new task
+// app.get('/api/v1/tasks/:id', (req, res) => {})             - get a single task 
+// app.patch('/api/v1/tasks/:id, (req, res) => {})            - update task
+// app.delete('/api/v1/tasks/:id, (req, res) => {})           - delete a single task
 
 const port = process.env.PORT || 3000;
 
